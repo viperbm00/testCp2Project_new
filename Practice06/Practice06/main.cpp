bc89aebb5c1683cb6109bf83d23ae191e0ede5ce
viperbm00 <viperbmjs@naver.com>
2016-11-27 19:51:33 +0900

    Frame class implement

--- Practice06/Practice06/main.cpp
+++ Practice06/Practice06/main.cpp
@@ -1,84 +1,18 @@
 #pragma once
-#include <process.h>
-#include <iostream>
-#include "KeyEvent.h"
-#include "Game.h"
+#include "Frame.h"
 
-using namespace std;
-
-/*unsigned int __stdcall mythread(void*)
+void main()
 {
-	int i = 0;
-	while (1)
+	try 
 	{
-		cout << GetCurrentThreadId() << ": " << i++ << endl;
-		Sleep(500);
-	}
-	
-	return 0;
-}
+		Frame *f1 = new Frame;
+		Frame *f2 = new Frame[10];
 
-unsigned int __stdcall keyEvent(void*)
-{
-	KeyEvent k;
-	int i;
-	while (1)
+		delete f1;
+		delete[] f2;
+	}
+	catch (string msg)
 	{
-		i = k.getkey();
-		switch (i)
-		{
-			case 37: cout << "Left key is pressed" << endl; break;
-			case 38: cout << "Up key is pressed" << endl; break;
-			case 39: cout << "Right key is pressed" << endl; break;
-			case 40: cout << "Down key is pressed" << endl; break;
-		}
+		cout << "Error msg : " << msg << endl;
 	}
-
-	return 0;
-}*/
-
-Game* game = new Game();
-unsigned int __stdcall mythread(void*)
-{
-	int i = 0;
-	while (1)
-	{
-		game->move();
-		Sleep(500);
-	}
-
-	return 0;
-}
-
-unsigned int __stdcall keyEvent(void*)
-{
-	KeyEvent k;
-	int i;
-	while (1)
-	{
-		i = k.getkey();
-		if (i != -1)
-		{
-			game->setDirection(i);
-		}
-	}
-
-	return 0;
-}
-
-int main()
-{
-	HANDLE handleA, handleB;
-
-	handleA = (HANDLE)_beginthreadex(0, 0, &mythread, (void*)0, 0, 0);
-	handleB = (HANDLE)_beginthreadex(0, 0, &keyEvent, (void*)0, 0, 0);
-
-	WaitForSingleObject(handleA, INFINITE);
-	WaitForSingleObject(handleB, INFINITE);
-
-	CloseHandle(handleA);
-	CloseHandle(handleB);
-
-	return 0;
-
 }
