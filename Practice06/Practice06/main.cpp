c43d28f767b8acb0b3b340748ee63edbfd4b04f4
viperbm00 <viperbmjs@naver.com>
2016-12-01 16:46:52 +0900

    STL practice ok

--- Practice06/Practice06/main.cpp
+++ Practice06/Practice06/main.cpp
@@ -1,7 +1,24 @@
 #pragma once
 #include <string>
-#include "Frame.h"
-#include "ExArray.h"
+#include <iostream>
+#include <vector>
+#include <list>
+#include <map>
+//#include "Frame.h"
+//#include "ExArray.h"
+
+using namespace std;
+
+void dump(list<string> &I)
+{
+	list<string>::iterator iter = I.begin();
+
+	while (iter != I.end())
+	{
+		cout << *iter << endl;
+		iter++;
+	}
+}
 
 void main()
 {
@@ -16,7 +33,7 @@ void main()
 	catch (string msg)
 	{
 		cout << "Error msg : " << msg << endl;
-	}*/
+	}
 
 	ExArray<int> arr1(3);
 	ExArray<char> arr2(4);
@@ -44,6 +61,65 @@ void main()
 
 	mySwap<ExArray<int>>(arr1, arr4);
 	arr1.printData();
-	arr4.printData();
+	arr4.printData();*/
+
+	//20161201 Chapter12
+
+	//STL-vector
+	/*
+	vector<int> v;
+
+	v.push_back(10);
+	v.push_back(20);
+	v.push_back(30);
+	v.insert(v.begin(), 40);
+	v.insert(v.end(), 50);
+
+	for (int i = 0; i < v.size(); i++)
+	{
+		cout << v[i] << endl;
+	}
+
+	cout << "access index at 1 : " << v.at(1) << endl;
+
+	v.erase(v.begin());
+	v.erase(v.end() - 1);
+	
+	vector<int>::iterator iter = v.begin();
+
+	cout << "access index at 1 with iterator : " << iter[1] << endl;
+
+	while (iter != v.end())
+	{
+		cout << *iter << endl;
+		iter++;
+	}*/
+
+	//STL-list
+
+	/*
+	list<string> names;
+	names.insert(names.begin(), "Konkuk");
+	names.insert(names.end(), "University");
+	names.insert(names.end(), "SCLAB");
+	names.insert(names.end(), "CSY");
+	dump(names);
+
+	cout << "===" << endl;
+	names.reverse();
+	dump(names);
+	*/
+
+	//STL-map
+	map<string, int> m;
+	m["seoul"] = 100;
+	m["daegu"] = 120;
+	m["busan"] = 200;
+
+	cout << "Train to Seoul = " << m["seoul"] << "$" << endl;
+	cout << "Train to Daegu = " << m["daegu"] << "$" << endl;
+	cout << "Train to Busan = " << m["busan"] << "$" << endl;
+
+
 
 }
